00b7f1475554fafdae971c8a6957d89f36be472e
Yuri Rublev <rubleviura34@hotmail.com>
2025-12-22 06:30:18 +0300

    Update Arch BIOS.m

--- Arch BIOS.m
+++ Arch BIOS.m
@@ -554,7 +554,7 @@ pacman -S --noconfirm man-db man-pages man-pages-YYYY
 pacman -S --noconfirm noto-fonts noto-fonts-emoji noto-fonts-cjk ttf-dejavu
 pacman -S --noconfirm iproute2 inetutils dnsutils
 pacman -S --noconfirm fastfetch hyfetch inxi
-pacman -S --noconfirm htop chromium qbittorrent libreoffice-fresh-ru archlinux-wallpaper
+pacman -S --noconfirm htop cpu-x chromium qbittorrent libreoffice-fresh-ru archlinux-wallpaper
 clear
 echo ""
 echo "######################################################################"
