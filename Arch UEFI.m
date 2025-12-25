4920f2a1d02c3db0d8f560cffe057ec7b0ec71d5
Yuri Rublev <rubleviura34@hotmail.com>
2025-12-25 16:16:42 +0300

    Update Arch UEFI.m

--- Arch UEFI.m
+++ Arch UEFI.m
@@ -545,7 +545,8 @@ pacman -S --noconfirm haveged
 systemctl enable haveged.service
 pacman -S --noconfirm wget usbutils lsof dmidecode dialog zip unzip unrar p7zip lzop lrzip sudo mlocate less bash-completion
 pacman -S --noconfirm dosfstools ntfs-3g exfatprogs gptfdisk fuse2 fuse3 fuseiso nfs-utils cifs-utils
-pacman -S --noconfirm ntfs3 # <-- Добавлено
+pacman -S --noconfirm dbus-broker
+systemctl enable dbus-broker.service
 pacman -S --noconfirm cronie
 systemctl enable cronie.service systemd-timesyncd.service
 echo 'vm.swappiness=10' > /etc/sysctl.d/99-swappiness.conf
